f8eea15dbc7051bd115f85d680e48de42a178265
Arghya Bandyopadhyay <72153788+arghyabandyopadhyay@users.noreply.github.com>
2025-06-17 20:57:41 +0530

    update

--- OtelDemoApp/obj/Debug/net9.0/OtelDemoApp.AssemblyInfo.cs
+++ OtelDemoApp/obj/Debug/net9.0/OtelDemoApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OtelDemoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ca84058b755b52e1bfe526b92811d0e52c462cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("OtelDemoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OtelDemoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
